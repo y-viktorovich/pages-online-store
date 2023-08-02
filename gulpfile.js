eaa3680b549fc1cce863cb75cd637398b2c622f4
Yurii <sfiwty@gmail.com>
2023-08-02 23:52:30 +0300

    add store page

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ function serve() {
     browserSync.init({
         server: {
             baseDir: "./" + distPath,
-            index: 'about.html'
+            index: 'store.html'
         }
     });
 }
